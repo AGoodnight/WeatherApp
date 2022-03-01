5a956fb2cafd6efc20094c2719a9eb77c75c0c1f
Adam Goodnight <adamgoodnight@Adams-MacBook-Pro.local>
2022-03-01 14:40:12 -0500

    minor tweak

--- src/app/features/weather/resolvers/current-conditions-resolver.ts
+++ src/app/features/weather/resolvers/current-conditions-resolver.ts
@@ -1,51 +1,61 @@
-import { Inject, Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, Resolve } from "@angular/router";
-import { Observable } from "rxjs";
-import { take } from "rxjs/operators";
+import { Inject, Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { select, Store } from '@ngrx/store';
-import { WeatherService } from "../weather.service";
-import { myCurrentWeatherSettingsSelector } from "../store/weather.selectors";
-import { WEATHER_FEATURE_DEFAULTS } from "../constants/weather.constants";
+import { WeatherService } from '../weather.service';
+import { myCurrentWeatherSettingsSelector } from '../store/weather.selectors';
+import { WEATHER_FEATURE_DEFAULTS } from '../constants/weather.constants';
 
 @Injectable()
-export class CurrentConditionsResolver implements Resolve<any>{
-    constructor(
-        @Inject(WeatherService) public weatherService:WeatherService,
-        @Inject(Store) public ngrxstore:Store<any>
-    ){}
-    
-    resolve(_route:ActivatedRouteSnapshot){
-        console.log('condition resolv');
-        return new Observable<WeatherReport>((obs)=>{
-            this.ngrxstore.pipe(select(myCurrentWeatherSettingsSelector)).subscribe((state)=>{
-            // First check for a saved location in thier local storage
-                if(!state.zip){
-                    navigator.geolocation.getCurrentPosition((location)=>{ 
-                        this.weatherService.getCurrentWeatherForLocation(
-                            <GeoCoordinates>{
-                                lat:location.coords.latitude,
-                                lon:location.coords.longitude
-                            },
-                            {units:state.unit || WEATHER_FEATURE_DEFAULTS.DEFAULT_UNITS}
-                        ).pipe(take(1)).subscribe((weather:WeatherReport)=>{
-                            obs.next(weather);
-                            obs.complete();
-                        })
-                    });
-                    // obs.next({} as WeatherReport);
-                    // obs.complete();
-                }else{
-                    this.weatherService.getCurrentWeatherForZip(
-                        state.zip,
-                        {units:state.unit || WEATHER_FEATURE_DEFAULTS.DEFAULT_UNITS}
-                    ).pipe(take(1)).subscribe((weather:WeatherReport)=>{
-                        this.weatherService.getWeatherIcon(weather.weather[0].icon).pipe(take(1)).subscribe((icon)=>{
-                            obs.next(weather);
-                            obs.complete();
-                        })
-                    });
-                }
-            })
+export class CurrentConditionsResolver implements Resolve<any> {
+  constructor(
+    @Inject(WeatherService) public weatherService: WeatherService,
+    @Inject(Store) public ngrxstore: Store<any>
+  ) {}
+
+  resolve(_route: ActivatedRouteSnapshot) {
+    console.log('condition resolv');
+    return new Observable<WeatherReport>((obs) => {
+      this.ngrxstore
+        .pipe(select(myCurrentWeatherSettingsSelector))
+        .subscribe((state) => {
+          // First check for a saved location in thier local storage
+          if (!state.zip) {
+            navigator.geolocation.getCurrentPosition((location) => {
+              this.weatherService
+                .getCurrentWeatherForLocation(
+                  <GeoCoordinates>{
+                    lat: location.coords.latitude,
+                    lon: location.coords.longitude,
+                  },
+                  {
+                    units: state.unit || WEATHER_FEATURE_DEFAULTS.DEFAULT_UNITS,
+                  }
+                )
+                .pipe(take(1))
+                .subscribe((weather: WeatherReport) => {
+                  obs.next(weather);
+                  obs.complete();
+                });
+            });
+          } else {
+            this.weatherService
+              .getCurrentWeatherForZip(state.zip, {
+                units: state.unit || WEATHER_FEATURE_DEFAULTS.DEFAULT_UNITS,
+              })
+              .pipe(take(1))
+              .subscribe((weather: WeatherReport) => {
+                this.weatherService
+                  .getWeatherIcon(weather.weather[0].icon)
+                  .pipe(take(1))
+                  .subscribe((icon) => {
+                    obs.next(weather);
+                    obs.complete();
+                  });
+              });
+          }
         });
-    }
+    });
+  }
 }
--- src/app/features/weather/resolvers/forecast.resolver.ts
+++ src/app/features/weather/resolvers/forecast.resolver.ts
@@ -1,49 +1,58 @@
-import { Inject, Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, Resolve } from "@angular/router";
-import { Observable, zip } from "rxjs";
-import { take, map } from "rxjs/operators";
+import { Inject, Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { Observable, zip } from 'rxjs';
+import { take, map } from 'rxjs/operators';
 import { select, Store } from '@ngrx/store';
-import { WeatherService } from "../weather.service";
-import { myCurrentWeatherSettingsSelector } from "../store/weather.selectors";
-import { WEATHER_FEATURE_DEFAULTS } from "../constants/weather.constants";
+import { WeatherService } from '../weather.service';
+import { myCurrentWeatherSettingsSelector } from '../store/weather.selectors';
+import { WEATHER_FEATURE_DEFAULTS } from '../constants/weather.constants';
 
 @Injectable()
-export class ForecastResolver implements Resolve<any>{
-    constructor(
-        @Inject(WeatherService) public weatherService:WeatherService,
-        @Inject(Store) public ngrxstore:Store<any>
-    ){}
-    
-    resolve(route:ActivatedRouteSnapshot){
-        console.log('forecast resolver');
-        return new Observable<ForecastReport>((obs)=>{
-            this.ngrxstore.pipe(select(myCurrentWeatherSettingsSelector)).subscribe((state)=>{
-                // First check for a saved location in thier local storage
-                if(!state.zip){
-                    navigator.geolocation.getCurrentPosition((location)=>{ 
-                        this.weatherService.getLocationForecast(
-                            <GeoCoordinates>{
-                                lat:location.coords.latitude,
-                                lon:location.coords.longitude
-                            },
-                            {units:state.unit || WEATHER_FEATURE_DEFAULTS.DEFAULT_UNITS, limit:WEATHER_FEATURE_DEFAULTS.DEFAULT_FORECAST_RANGE}
-                        ).pipe(take(1)).subscribe((forecast:ForecastReport)=>{
-                            obs.next(forecast);
-                            obs.complete();
-                        })
-                    });
-                    // obs.next({} as ForecastReport);
-                    // obs.complete();
-                }else{
-                    this.weatherService.getZipForecast(
-                        state.zip,
-                        {units:state.unit || WEATHER_FEATURE_DEFAULTS.DEFAULT_UNITS, limit:WEATHER_FEATURE_DEFAULTS.DEFAULT_FORECAST_RANGE}
-                    ).pipe(take(1)).subscribe((forecast:ForecastReport)=>{
-                        obs.next(forecast);
-                        obs.complete();
-                    })
-                }
+export class ForecastResolver implements Resolve<any> {
+  constructor(
+    @Inject(WeatherService) public weatherService: WeatherService,
+    @Inject(Store) public ngrxstore: Store<any>
+  ) {}
+
+  resolve(route: ActivatedRouteSnapshot) {
+    console.log('forecast resolver');
+    return new Observable<ForecastReport>((obs) => {
+      this.ngrxstore
+        .pipe(select(myCurrentWeatherSettingsSelector))
+        .subscribe((state) => {
+          // First check for a saved location in thier local storage
+          if (!state.zip) {
+            navigator.geolocation.getCurrentPosition((location) => {
+              this.weatherService
+                .getLocationForecast(
+                  <GeoCoordinates>{
+                    lat: location.coords.latitude,
+                    lon: location.coords.longitude,
+                  },
+                  {
+                    units: state.unit || WEATHER_FEATURE_DEFAULTS.DEFAULT_UNITS,
+                    limit: WEATHER_FEATURE_DEFAULTS.DEFAULT_FORECAST_RANGE,
+                  }
+                )
+                .pipe(take(1))
+                .subscribe((forecast: ForecastReport) => {
+                  obs.next(forecast);
+                  obs.complete();
+                });
             });
+          } else {
+            this.weatherService
+              .getZipForecast(state.zip, {
+                units: state.unit || WEATHER_FEATURE_DEFAULTS.DEFAULT_UNITS,
+                limit: WEATHER_FEATURE_DEFAULTS.DEFAULT_FORECAST_RANGE,
+              })
+              .pipe(take(1))
+              .subscribe((forecast: ForecastReport) => {
+                obs.next(forecast);
+                obs.complete();
+              });
+          }
         });
-    }
+    });
+  }
 }
