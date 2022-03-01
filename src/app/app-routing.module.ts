13e76c70be3b1d3a16eda3127a232d4e5c739911
Adam Goodnight <adamgoodnight@Adams-MacBook-Pro.local>
2022-03-01 14:35:39 -0500

    fixed tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { DayView } from './features/weather/day-view/day-view.component';
 import { CurrentConditionsResolver } from './features/weather/resolvers/current-conditions-resolver';
 
-const routes: Routes = [
-  {path:'',component:DayView,resolve:{
-      currentConditions:CurrentConditionsResolver
-    }
-  }
+export const routes: Routes = [
+  {
+    path: '',
+    component: DayView,
+    resolve: {
+      currentConditions: CurrentConditionsResolver,
+    },
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,79 @@
-import { TestBed } from '@angular/core/testing';
+/// <reference path="../../node_modules/@types/jasmine/index.d.ts" />
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { Action } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { WeatherService } from './features/weather/weather.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { NAVIGATION_CONFIG } from './shared/navigation/navigation.interface';
+import { TopNavComponent } from './shared/navigation/top-nav/top-nav.component';
+import { ToastComponent } from './shared/notification/toasts/toast.component';
+import { initialState } from './shared/navigation/store/navigation.reducer';
+import { weatherRoutes } from './features/weather/weather.routing';
+import { routes } from './app-routing.module';
+import { WEATHER_PATHS } from './features/weather/constants/weather.constants';
+
+// Mock the Injection Token
+const NAVIGATION_CONFIG_PROVIDER = [
+  {
+    provide: NAVIGATION_CONFIG,
+    useValue: {
+      routes: WEATHER_PATHS,
+    },
+  },
+];
 
 describe('AppComponent', () => {
+  let actions$: Observable<Action>;
+  // let httpMock: HttpTestingController;
+  // let weatherService: WeatherService;
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule.withRoutes([...routes, ...weatherRoutes]),
+        HttpClientTestingModule,
       ],
-      declarations: [
-        AppComponent
+      declarations: [AppComponent, TopNavComponent, ToastComponent],
+      providers: [
+        provideMockActions(() => actions$),
+        provideMockStore({
+          initialState,
+        }),
+        WeatherService,
+        NAVIGATION_CONFIG_PROVIDER,
       ],
     }).compileComponents();
+
+    // weatherService = TestBed.inject(WeatherService);
+    // httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  // Neccessary to create component before each test
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+  });
+
+  // Important, removes old state
+  afterEach(() => {
+    fixture.destroy();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'weatherApp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.title).toEqual('weatherApp');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('weatherApp app is running!');
   });
 });
